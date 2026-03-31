1aea5beb4709a72acab8b7dbb742f4c8397a12a6
Ilkin Ismayilov <ismayilovilkin2006@gmail.com>
2026-03-31 17:15:41 +0400

    Entity classes and enum added

---
+++ SpringDataJPA/src/main/java/org/example/springdatajpa/entity/FlightBooking.java
@@ -0,0 +1,51 @@
+package org.example.springdatajpa.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "flight_bookings")
+public class FlightBooking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int bookingId;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", referencedColumnName = "customerId")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "flight_id", referencedColumnName = "flightId")
+    private Flight flight;
+
+    public FlightBooking() {
+    }
+
+    public FlightBooking(Customer customer, Flight flight) {
+        this.customer = customer;
+        this.flight = flight;
+    }
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+}
---
+++ SpringDataJPA/src/main/java/org/example/springdatajpa/enums/CustomerStatus.java
@@ -0,0 +1,7 @@
+package org.example.springdatajpa.enums;
+
+public enum CustomerStatus {
+    GOLD,
+    SILVER,
+    NONE
+}
